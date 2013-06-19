0f71f84e80e9fdebb6e56742050787b6f14b9f5b
Luke Robertson <luke.robertson@mac.com>
2013-06-19 22:53:28 +0100

    properly passing params in to methods

--- subway.rb
+++ subway.rb
@@ -1,18 +1,16 @@
 puts `clear`
-def trains
-  {
+trains = {
     :l => ["8th-l", "6th-l", "union", "3rd-l", "1st-l"],
     :n => ["8th-n", "union", "23rd-n", "28th-n", "34th-n", "ts"],
     :s => ["ap", "union", "23rd-s", "28th-s", "33rd-s", "gc"],
   }
-end
 
-def line_choice
+def line_choice(lines)
   condition = true
   while condition
     puts "Your options are: L, N, S(ix):"
     response = gets.chomp.downcase.to_sym
-    if trains.keys.include?(response)
+    if lines.keys.include?(response)
           condition = false
         else
       puts "I don't understand... please type your response exactly as shown."
@@ -21,11 +19,12 @@ def line_choice
   response
 end
 
-def station_choice(x)
+def station_choice(lines, station)
 condition = true
   while condition
+    puts "\nPlease choose a station: #{(lines[station]).join(", ")}"
     response = gets.chomp.downcase
-    if trains[x].include?(response)
+    if lines[station].include?(response)
       on_station =  response
       condition = false
     else
@@ -37,28 +36,23 @@ end
 
 puts "JOURNEY STOP CALCULATOR"
 puts "\nWhich train-line would you like to join?"
-on_line = line_choice
+on_line = line_choice(trains)
 
-puts "\nPlease choose a station from which to board the train: #{(trains[on_line]).join(", ")}"
-on_station = station_choice(on_line)
+on_station = station_choice(trains, on_line)
 
 puts "\nWhich train-line would you like to exit?"
-off_line = line_choice
+off_line = line_choice(trains)
 
-puts "\nPlease choose a station from which to exit the train: #{(trains[off_line]).join(", ")}"
-off_station = station_choice(off_line)
+off_station = station_choice(trains, off_line)
 
 if on_station == off_station
  puts "You don't need to catch the train.... you are already at your destination!"
 else
-
-#journey to union
   to_union = (trains[on_line].index(on_station)) - (trains[on_line].index("union"))
   if to_union < 0
     to_union *= -1
   end
 
-#journey from union
   from_union = trains[off_line].index(off_station) - trains[off_line].index("union")
   if from_union < 0
     from_union *= -1
