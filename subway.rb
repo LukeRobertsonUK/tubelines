6d5aa81a8632e4ba95ec86a09db7a6931fd127ed
Luke Robertson <luke.robertson@mac.com>
2013-06-19 20:36:05 +0100

    added while loops to stop unrecognised responses

--- subway.rb
+++ subway.rb
@@ -5,17 +5,59 @@ trains[:n] = ["8th-n", "union", "23rd-n", "28th-n", "34th-n", "ts"]
 trains[:s] = ["ap", "union", "23rd-s", "28th-s", "33rd-s", "gc"]
 
 puts "JOURNEY STOP CALCULATOR"
-puts "\nWhich train-line would you like to join?"
-puts "Your options are: L, N, S(ix):"
-on_line = gets.chomp.downcase.to_sym
-puts "\nPlease choose a station to board the train from: #{(trains[on_line]).join(", ")}"
-on_station = gets.chomp.downcase
+condition1 = true
+while condition1
+  puts "\nWhich train-line would you like to join?"
+  puts "Your options are: L, N, S(ix):"
+  response = gets.chomp.downcase.to_sym
+  if trains.keys.include?(response)
+    on_line = response
+    condition1 = false
+  else
+    puts "I don't understand... please type your response exactly as shown."
+    puts
+  end
+end
 
-puts "\nWhich train-line would you like to exit?"
-puts "Your options are: L, N, S(ix):"
-off_line = gets.chomp.downcase.to_sym
-puts "\nPlease choose a station to exit the train from: #{(trains[off_line]).join(", ")}"
-off_station = gets.chomp.downcase
+condition2 = true
+while condition2
+  puts "\nPlease choose a station from which to board the train: #{(trains[on_line]).join(", ")}"
+  response = gets.chomp.downcase
+  if trains[on_line].include?(response)
+    on_station =  response
+    condition2 = false
+  else
+    puts "I don't understand... please type your response exactly as shown."
+    puts
+  end
+end
+
+condition3 = true
+while condition3
+  puts "\nWhich train-line would you like to exit?"
+  puts "Your options are: L, N, S(ix):"
+  response = gets.chomp.downcase.to_sym
+  if trains.keys.include?(response)
+    off_line = response
+    condition3 = false
+  else
+    puts "I don't understand... please type your response exactly as shown."
+    puts
+  end
+end
+
+condition4 = true
+while condition4
+  puts "\nPlease choose a station to exit the train from: #{(trains[off_line]).join(", ")}"
+  response = gets.chomp.downcase
+  if trains[off_line].include?(response)
+    off_station = response
+    condition4 = false
+  else
+    puts "I don't understand... please type your response exactly as shown."
+    puts
+  end
+end
 
 #journey to union
 to_union = (trains[on_line].index(on_station)) - (trains[on_line].index("union"))
